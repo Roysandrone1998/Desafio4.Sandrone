3bc9f4fa12fb497b164cc7e9530a80834aec0385
Roysandrone1998 <raysandrone@gmail.com>
2024-02-06 18:21:58 -0300

    trabajandoo

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 import express from "express";
 
-const app = express()
-const port = 8080
+const app = express();
+const port = 8080;
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Ruta de prueba
+app.get("/", (req, res) => {
+  res.send("¡Hola, mundo!");
+});
 
-
-
-app.listen(port,() => {
-    console.log("Server listening in port: " + port)
-})
+app.listen(port, () => {
+  console.log("Server listening on port: " + port);
+});
